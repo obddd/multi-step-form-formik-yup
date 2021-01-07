31ae705af592bdfc81738f900482958280042b61
obddd <obaidulhaq20@gmail.com>
2021-01-07 21:49:35 +0500

    created form with fields

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,43 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Card, CardContent } from '@material-ui/core';
+import { Formik, Form, Field } from 'formik';
+import { TextField, CheckboxWithLabel } from 'formik-material-ui';
 
 export default function Home() {
   return (
     <Card>
       <CardContent>
-        <Typography variant="h1">Hello Youtube!!!</Typography>
+        <Formik
+          initialValues={{
+            firstname: '',
+            lastname: '',
+            millionaire: false,
+            money: '',
+            description: '',
+          }}
+          onSubmit={() => {}}
+        >
+          <Form autoComplete="off">
+            <Field name="firstname" component={TextField} label="First Name" />
+            <Field name="lastname" component={TextField} label="Last Name" />
+            <Field
+              type="checkbox"
+              name="millionaire"
+              component={CheckboxWithLabel}
+              Label={{ label: 'I am a millionaire!' }}
+            />
+            <Field
+              type="number"
+              name="money"
+              component={TextField}
+              label="I have Money"
+            />
+            <Field
+              name="description"
+              component={TextField}
+              label="Description"
+            />
+          </Form>
+        </Formik>
       </CardContent>
     </Card>
   );
